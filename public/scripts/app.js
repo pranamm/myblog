50780f848e8bddba9495acd6cc90f1056eafa81c
Pranam Mohanty <pranam.mca@gmail.com>
2016-01-07 18:22:01 +0530

    restructured client side applicaiton

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,58 +1,29 @@
-/* 
- * @Author: pranam
- * @Date:   2014-12-19 23:39:43
- * @Last Modified by:   pranam
- * @Last Modified time: 2015-01-20 10:58:12
- */
-
 'use strict';
-var app = angular.module('app', ['ui.bootstrap', 'ngRoute']);
+var app = angular.module('app', [
+    //'ngSanitize',
+    'ui.bootstrap',
+    'ui.router',
+    'gist',
 
+    'app.common',
+    'app.posts',
+    'app.profile'
+]);
 
-app.config(['$routeProvider', '$locationProvider',
-    function($routeProvider, $locationProvider) {
-        $routeProvider.otherwise('home');
+var commonModule = angular.module('app.common', []),
+    postsModule = angular.module('app.posts', []),
+    profileModule = angular.module('app.profile', []);
 
-        $routeProvider.when("/home/", {
-            templateUrl: '/views/post/home.html',
+app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
+    function($stateProvider, $urlRouterProvider, $httpProvider) {
+        'use strict';
+
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider.state("home", {
+            url: "/home",
+            templateUrl: '../views/home.html',
             controller: 'HomeCtrl'
         })
-
-        .when("/admin", {
-            templateUrl: '/views/admin/admin.html',
-            controller: 'AdminCtrl'
-        })
-
-        .when("/admin/posts", {
-            templateUrl: '/views/post/posts.html',
-            controller: 'PostsCtrl'
-        })
-
-        .when("/admin/posts/create", {
-            templateUrl: '/views/post/create-post.html',
-            controller: 'CreatePostCtrl'
-        })
-
-        .when("admin/profile", {
-            templateUrl: '/views/profile/profile.html',
-            controller: 'ProfileCtrl'
-        })
-
-        /*$locationProvider.html5Mode({
-            enabled: true,
-            requireBase: false
-        }).hashPrefix('!');*/
-
-        $locationProvider.html5Mode(false);
-
-        /*Users Routes*/
-        /*.state('home.users', {
-            url: '',
-            abstract: true,
-            templateUrl: '../warehouse/views/users/users.html',
-            data: {
-                authorizedRoles: [USER_ROLES.admin]
-            }
-        })*/
     }
 ]);
---
+++ public/scripts/controllers/common/app-ctrl.js
@@ -0,0 +1,68 @@
+commonModule.controller('AppCtrl', ['$scope', '$location', '$rootScope', '$timeout',
+    function($scope, $location, $rootScope, $timeout) {
+        'use strict';
+
+        $rootScope.loggedInStatus = false;
+
+        $rootScope.notification = "";
+        $rootScope.notificationType = "";
+        $rootScope.notificationHidden = "hidden";
+
+        /*$scope.logout = function() {
+            AuthService.logout().then(function(user) {
+                $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
+            }, function() {
+                $rootScope.$broadcast(AUTH_EVENTS.logoutFailed);
+            });
+        };
+
+        $scope.$on(AUTH_EVENTS.loginSuccess, function(event) {
+            $rootScope.loggedInStatus = true;
+            if ($location.url() === "/login") {
+                $location.path('/home');
+            }
+        });
+
+        $scope.$on(AUTH_EVENTS.notAuthenticated, function(event, msg) {
+            $rootScope.loggedInStatus = false;
+            $location.path('/login');
+            $rootScope.$broadcast('notification', {
+                message: msg,
+                type: "danger"
+            }, false);
+        });
+
+        $scope.$on(AUTH_EVENTS.logoutSuccess, function(event) {
+            $rootScope.loggedInStatus = false;
+            $location.path('/login');
+        });
+
+        $scope.$on(AUTH_EVENTS.logoutFailed, function(event) {
+
+        });
+
+        $scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
+            $location.path('/home');
+        });
+
+        $scope.$on("notification", function(event, data, timeoutNeeded) {
+            $rootScope.notification = data.message;
+            $rootScope.notificationType = data.type;
+            $rootScope.notificationHidden = "";
+            if (timeoutNeeded) {
+                $timeout(function() {
+                    $rootScope.notification = "";
+                    $rootScope.notificationType = "";
+                    $rootScope.notificationHidden = "hidden";
+                }, 5000);
+            }
+
+        });
+
+        $scope.$on("hideNotification", function(event) {
+            $rootScope.notification = "";
+            $rootScope.notificationType = "";
+            $rootScope.notificationHidden = "hidden";
+        });*/
+    }
+]);
